23df254e2da7f14dfc68b24b813836130f21f909
Juan Cuadra <jcb8793@gmail.com>
2017-12-04 21:09:21 +0100

    Add Components/Services to App Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Services
+import { AuthService } from './services/auth/auth.service';
+import { UserService } from './services/user/user.service';
+import { OdeService } from './services/ode/ode.service';
+
 
 import { AppComponent } from './app.component';
+
+// Pages
 import { PageLoginComponent } from './pages/page-login/page-login.component';
 import { PageShareOdeComponent } from './pages/page-share-ode/page-share-ode.component';
-import { OdeFormComponent } from './components/ode-form/ode-form.component';
-import { BrowseComponent } from './pages/browse/browse.component';
-import { OdeBoxComponent } from './components/ode-box/ode-box.component';
-import { PageOdeInfoComponent } from './pages/page-ode-info/page-ode-info.component';
-import { SpotifyInfoComponent } from './components/spotify-info/spotify-info.component';
-import { FullLyricsComponent } from './components/full-lyrics/full-lyrics.component';
-import { GeniusFactComponent } from './components/genius-fact/genius-fact.component';
-import { PageUserProfileComponent } from './pages/page-user-profile/page-user-profile.component';
-import { UserInfoComponent } from './components/user-info/user-info.component';
-import { OdeListComponent } from './components/ode-list/ode-list.component';
 import { PageBrowseComponent } from './pages/page-browse/page-browse.component';
+import { PageOdeInfoComponent } from './pages/page-ode-info/page-ode-info.component';
+import { PageUserProfileComponent } from './pages/page-user-profile/page-user-profile.component';
+
+// Components
+import { OdeFormComponent } from './components/ode-form/ode-form.component';
+import { OdeBoxComponent } from './components/ode-box/ode-box.component';
+import { OdeListComponent } from './components/ode-list/ode-list.component';
 import { OdeSpotifyInfoComponent } from './components/ode-spotify-info/ode-spotify-info.component';
 import { OdeFullLyricsComponent } from './components/ode-full-lyrics/ode-full-lyrics.component';
 import { OdeGeniusFactComponent } from './components/ode-genius-fact/ode-genius-fact.component';
+import { UserInfoComponent } from './components/user-info/user-info.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+
 
 
 @NgModule({
@@ -27,26 +33,22 @@ import { NavBarComponent } from './components/nav-bar/nav-bar.component';
     AppComponent,
     PageLoginComponent,
     PageShareOdeComponent,
-    OdeFormComponent,
-    BrowseComponent,
-    OdeBoxComponent,
-    PageOdeInfoComponent,
-    SpotifyInfoComponent,
-    FullLyricsComponent,
-    GeniusFactComponent,
-    PageUserProfileComponent,
-    UserInfoComponent,
-    OdeListComponent,
     PageBrowseComponent,
+    PageOdeInfoComponent,
+    PageUserProfileComponent,
+    OdeFormComponent,
+    OdeBoxComponent,
+    OdeListComponent,
     OdeSpotifyInfoComponent,
     OdeFullLyricsComponent,
     OdeGeniusFactComponent,
-    NavBarComponent
+    UserInfoComponent,
+    NavBarComponent,
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ AuthService, UserService, OdeService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
